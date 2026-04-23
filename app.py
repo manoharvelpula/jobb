9f71edf20e7b5c4d7b18cfdba11e1bcf38da1db8
manoharvelpula <vbunny2005@gmail.com>
2026-04-23 19:52:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import io
 # ---------------- PAGE CONFIG ----------------
 st.set_page_config(page_title="AI Powered Career Path Analyzer", layout="wide")
 
-st.title("🚀 AI Career Intelligence PRO")
+st.title("🚀 AI Powered Career Path Analyzer")
 st.write("Next-Gen Career Analyzer with AI Insights")
 
 # ---------------- SESSION STATE ----------------
